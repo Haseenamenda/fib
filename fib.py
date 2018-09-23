36259f689efd68875636403e2d3504e8c86eb7ce
Haseenamenda <41771159+Haseenamenda@users.noreply.github.com>
2018-09-23 11:31:14 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,9 +1,9 @@
-def printFibonacciseries(n):
+def printFibonacciseries(N):
 	f1=0
 	f2=1
-	if(n<1):
+	if(N<1):
 		return
-	for x in range(0,n):
+	for x in range(0,N):
 		print(f2,end=" ")
 		next=f1+f2
 		f1=f2
